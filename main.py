096e5ee6274d53d57b4af76edb07bb2cb13b6bd6
Jakob Torben <jakob_torben@hotmail.com>
2020-06-18 15:42:38 +0200

    updated function names to main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     model = shortest_path(7, 30, [1, 0], [5, 6])        #Run the shortest_path program
     
     model.grid_gen()
-    model.max_pi(1000)  
+    model.optimal_path(100)   
 
 
 

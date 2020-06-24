eabd71474d4453d6e485be4b62012284b1d61384
tadjer92 <66951171+tadjer92@users.noreply.github.com>
2020-06-24 09:53:24 +0200

    Add files via upload

--- test/main.py
+++ test/main.py
@@ -4,18 +4,18 @@ Created on Sat Jun 20 13:04:12 2020
 
 @author: 2920029
 """
+
 import gym
-import env.smallVaringGoalRep1Environment
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines import DQN
 
+import env.smallVaringGoalRep1Environment
 env = gym.make('SmallVaringGoalRep1Env-v0')
 
-from stable_baselines.deepq.policies import MlpPolicy
 model = DQN(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=25000)
+model.learn(total_timesteps=50000)
 
-
-# Enjoy trained agent
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)

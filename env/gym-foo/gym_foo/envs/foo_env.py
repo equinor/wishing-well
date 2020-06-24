61c8bde12d49e10680ad5d6c288b6746059d3320
torsteinotterlei <torstein@otterlei.no>
2020-06-24 09:52:54 +0200

    Updates to openai gym interface

--- env/gym-foo/gym_foo/envs/foo_env.py
+++ env/gym-foo/gym_foo/envs/foo_env.py
@@ -19,19 +19,21 @@ class FooEnv(gym.Env):
         self.subplot = self.fig.add_subplot(111)
 
         self.reward_dict = {}
-        self.actions_dict = {0:(-1,0), 1:(0,1), 2:(1,0)}
-
-        # Default values
-        self.grid_size = 7
-        self.distance_points = 20
-
-        init_state = (1, 0)
-        end_state = (5,6)
+        self.actions_dict = {0:(-1,0), 1:(0,1), 2:(1,0)}        #Actions: 0:Left, 1:Down, 2:Right
 
         self.init_state = None
         self.end_state = None
         self.current_state = None
         self.next_state = None
+
+        # Default values
+        self.grid_width = 7
+        self.grid_height = 7
+        self.distance_points = 20
+        self.default_reward = 0                      #This is the default reward for an action that has not been assigned another by the agent yet
+
+        init_state = (1, 0)
+        end_state = (5,6)
 
         self.set_init_end_state(init_state,end_state)       #Also makes a call to update_plot
 
@@ -44,15 +46,16 @@ class FooEnv(gym.Env):
         self.update_plot()
 
     #Method for setting grid-size other than default
-    def set_grid_size(self,size):
-        self.grid_size = size
+    def set_grid_size(self,width,height):
+        self.grid_width = width
+        self.grid_height = height
         self.update_plot()
 
     #Method for updating plot in case user edits parameters from default
     def update_plot(self):
         self.subplot.cla()
-        x = np.arange(0, self.grid_size*self.distance_points, self.distance_points)
-        y = np.arange(0, self.grid_size*self.distance_points, self.distance_points)
+        x = np.arange(0, self.grid_width*self.distance_points, self.distance_points)
+        y = np.arange(0, self.grid_height*self.distance_points, self.distance_points)
         X, Y = np.meshgrid(x, y)
         
         self.subplot.plot(X, Y, 'ob')
@@ -72,23 +75,20 @@ class FooEnv(gym.Env):
         if state in self.reward_dict:
             return self.reward_dict[state]
         else:
-            self.reward_dict[state] = 0
-            return 0
+            self.reward_dict[state] = self.default_reward
+            return self.default_reward
 
     #Allows you to set reward for specified state
     def set_reward(self, state, reward):
         self.reward_dict[state] = reward
 
 
-    #Actions: 0:Left, 1:Down, 2:Right
     def step(self, action):
-        done = False
         action = self.scale_point(self.actions_dict[action])
 
         self.current_state = (self.current_state[0]+action[0], self.current_state[1]+action[1])
 
-        if self.current_state == self.end_state:
-            done = True
+        done = self.current_state == self.end_state
 
         reward = self.get_reward(self.current_state)
             
@@ -134,13 +134,11 @@ class FooEnv(gym.Env):
 
 #env.set_grid_size(10)
 #env.set_init_end_state((1, 0),(5, 9))
-'''
-path = []
-path.append(env.init_state)
-path.append(env.step(0)[0])
-path.append(env.step(1)[0])
-path.append(env.step(2)[0])
-print(path)
-env.plot_path(path)
-env.close()
-'''
+#path = []
+#path.append(env.init_state)
+#path.append(env.step(0)[0])
+#path.append(env.step(1)[0])
+#path.append(env.step(2)[0])
+#print(path)
+#env.plot_path(path)
+#env.close()

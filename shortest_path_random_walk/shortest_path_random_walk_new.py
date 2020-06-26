0efc3322e11a8fa080e27765a1df73a87ce4edda
torsteinotterlei <torstein@otterlei.no>
2020-06-26 13:20:43 +0200

    Fixed gym interface

--- shortest_path_random_walk/shortest_path_random_walk_new.py
+++ shortest_path_random_walk/shortest_path_random_walk_new.py
@@ -6,7 +6,6 @@ Created on Thu Jun 18 08:48:53 2020
 """
 
 import matplotlib as mpl            #Import entire matplotlib and use WebAgg for showing in browser
-#from matplotlib.figure import Figure
 
 import matplotlib.pyplot as plt     #Still use plt for pyplot for familiarity
 import numpy as np
@@ -15,75 +14,74 @@ import gym
 
 #from gym import envs
 from gym_ww import envs
-
-
-#all_envs = envs.registry.all()
-#env_ids = [env_spec.id for env_spec in all_envs]
-#print(env_ids)
+from stable_baselines.common.env_checker import check_env
 
 #gym.make('well-plot-3-v0')
 
 class shortest_path:
     def __init__(self):
         self.env = gym.make('well-plot-5-v0')
-        self.iteration = 0
         self.policy = []
-        self.policies = []
 
+        self.num_tries = 5
+        self.max_len_path = 15
     
     def game(self):
-        max_it = 10000
-        policy = [self.env.current_state]
+        self.policy = [self.env.state]
+        for i in range(self.max_len_path):
 
-        # take iteration number of random steps to reach ending point
-        while self.iteration <= max_it:
-            action = self.env.action_space.sample()
-
-            #while not self.valid_action(action):                        #Keep choosing random action until it finds valid action
-            #    action = self.env.action_space.sample()
-
-            new_state, reward, done, info = self.env.step(action)
-            self.iteration += 1
-            policy.append(new_state)
-            
-            if done:
-                self.policy = policy
-                print("End position found in ", self.iteration, "iterations")
+            if not self.possible_to_move(self.env.state):
                 break
 
-            if self.iteration == max_it:
-                print("Shortest path not found in ", max_it, " iterations")
+            action = self.env.action_space.sample()
+            while not self.valid_action(action):                        #Keep choosing random action until it finds valid action
+                action = self.env.action_space.sample()
 
+            new_state, reward, done, info = self.env.step(action)
+            self.policy.append(new_state)
+
+            if done:
+                print("Found path of length ", i+1)
+                return self.policy
+                
+        print("Did not find path shorter than", self.max_len_path)
+        return None
+        
 
     def optimal_path(self):
-        num_games = 100
-        valuefunc = np.zeros(num_games)
+        policies = []
         
-        #for i in range(num_games):
-        #    self.game()
-        #    self.env.reset()
-        #
-        #    valuefunc[i] = -self.iteration
-        #    self.policies.append(self.policy)
-        self.policy.append(self.env.init_state)
-        self.policy.append(self.env.step(1)[0])
-        self.policy.append(self.env.step(1)[0])
-        self.policy.append(self.env.step(1)[0])
-        self.policy.append(self.env.step(1)[0])
-        self.policy.append(self.env.step(1)[0])
-        self.policies.append(self.policy)
+        for i in range(self.num_tries):
+            policy = self.game()
+            if not policy == None:
+                policies.append(policy)
+            self.env.reset()
 
-        #index = np.where(valuefunc == max(valuefunc) )[0][0]
-        index = 0
-        shortest_path = self.policies[index]
-        #print("Path length: ", -max(valuefunc))
-        self.env.plot_path(shortest_path)
+        policies = [ list(policy) for policy in policies ]
+        policies.sort(key=len)
+        shortest_policy = policies[0]
+        print("Length of shortest path found: ", len(shortest_policy)-1)
+        self.env.plot_path(shortest_policy)
+
 
     #Check if action is in board. Can be replaced by negative rewards when using RL: env.set_reward(state,reward)
     def valid_action(self,action):
         new_state = self.env.check_step(action)
-        return (0 <= new_state[0] <= self.env.grid_width*self.env.distance_points) and (0 <= new_state[1] <= self.env.grid_height*self.env.distance_points)
+        valid_state = self.env.valid_state(new_state)
+        policy_list = [ list(policy) for policy in self.policy ]
+        new_state_list = list(new_state)
+        not_prev_state = new_state_list not in policy_list
+        return valid_state and not_prev_state
 
-
+    def possible_to_move(self,state):
+        for action in range(self.env.action_space.n):
+            new_state = self.env.check_step(action)
+            policy_list = [ list(policy) for policy in self.policy ]
+            new_state_list = list(new_state)
+            valid_state = self.env.valid_state(new_state) and new_state_list not in policy_list
+            if valid_state:
+                return True
+        return False
+    
     def get_figure(self):
         return self.env.fig
